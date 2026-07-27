3e9b4e87fb0bee8f0d7bafd69a28b21d79164ee3
OffSkull <cherepov_m@list.ru>
2026-07-27 15:41:14 +0600

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -8,7 +8,7 @@
   Никогда не вставляйте сюда service_role key.
 */
 window.OFFSKULL_SUPABASE_CONFIG = {
-  url: "PASTE_YOUR_SUPABASE_PROJECT_URL",
-  publishableKey: "PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY",
+  url: "https://qrdgnovuunruljpsffmk.supabase.co",
+  publishableKey: "sb_publishable_9Gsy0Pu8jutwxQxxZdEjAQ_1gBpITCM",
   bucket: "offskull-media"
 };
